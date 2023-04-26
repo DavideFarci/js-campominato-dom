9c5d9199b271374e69b2fd70ec7f99c4ce79b197
DavideFarci <yank92.df@gmail.com>
2023-04-26 15:52:06 +0200

    tutto scibile umano

--- script/functions.js
+++ script/functions.js
@@ -5,3 +5,14 @@ function createGrid(numCells, eleContainer) {
 		eleContainer.innerHTML += `<div class="cell">${i}</div>`;
 	}
 }
+
+
+function getRandom (min, max, numbers) {
+	while (numbers.length < 16) {
+  	let randomNumber = Math.floor(Math.random() * (max - min + 1) ) + min;
+  	if (!numbers.includes(randomNumber)) {
+    numbers.push(randomNumber);
+  }
+}
+console.log(numbers);
+}
--- script/script.js
+++ script/script.js
@@ -22,6 +22,8 @@ Le validazioni e i controlli possiamo farli anche in un secondo momento.
 //selezionare il bottone per creare la griglia 
 const eleButton = document.querySelector("#btn");
 
+//Array per le bombe
+const num = [];
 
 eleButton.addEventListener("click", function () {
     const eleGrid = document.querySelector('.grid');
@@ -29,32 +31,41 @@ eleButton.addEventListener("click", function () {
     let value = eleDifficulty.options[eleDifficulty.selectedIndex].value;
     console.log(value);
     
+    
     // generare la griglia in base alla difficoltà scelta
     if (value == "100") {
         eleGrid.classList.remove("grid_easy", "grid_medium", "grid_hard");
         eleGrid.classList.add("grid_easy");
         createGrid(100, eleGrid);
+        getRandom(1, 100, num);
     } else if (value == "81") {
         eleGrid.classList.remove("grid_easy", "grid_medium", "grid_hard");
         eleGrid.classList.add( "grid_medium");
         createGrid(81, eleGrid);
+        getRandom(1, 81, num);
     } else if (value == "49") {
         eleGrid.classList.remove("grid_easy", "grid_medium", "grid_hard");
         eleGrid.classList.add("grid_hard");
         createGrid(49, eleGrid);
+        getRandom(1, 49, num);
     }
-
+    
+    // debugger
     const listCells = document.querySelectorAll('.cell');
     // applicare gli event listeners a tutte le celle della griglia
     for (let i = 0; i < listCells.length; i++) {
         const cell = listCells[i];
         cell.addEventListener('click', function() {
-            console.log("hai cliccato la cella " + this.innerHTML)
-            this.classList.toggle('clicked');
+            if (num.includes(i + 1)) {
+                console.log("hai cliccato la cella " + this.innerHTML);
+                this.classList.toggle("bomb");
+                console.log("hai perso");
+            } else {
+                console.log("hai cliccato la cella " + this.innerHTML);
+                this.classList.toggle('clicked');
+            }
         })
     }
-
-    
     
 });
 
